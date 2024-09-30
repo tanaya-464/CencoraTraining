b10a7f5f01eff0d6314f0d51f6e3c6e2f39c66d7
tanaya-464 <109287086+tanaya-464@users.noreply.github.com>
2024-09-30 15:36:32 +0530

    Update CiCdexampleApplication.java

--- src/main/java/com/example/demo/CiCdexampleApplication.java
+++ src/main/java/com/example/demo/CiCdexampleApplication.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-
+@SpringBootConfigaration
 public class CiCdexampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CiCdexampleApplication.class, args);
